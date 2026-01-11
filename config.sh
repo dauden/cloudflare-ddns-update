5e6e38ef0a16b25f085cabee1c240dbb66902eab
BlackBean <anh.nguyen@codingland.com>
2026-01-11 17:44:53 +0700

    Support multiple records

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 declare API_KEY="your_copied_cloudflare_api_key"
 declare ACCOUNT="your_email"
 declare ZONE_ID="your_domain_zone_id"
-declare RECORD_NAME="your_domain_zone_name"
+declare RECORD_NAMES="your_domain_zone_0_name your_domain_zone_1_name"
 
 # Options changed before deploy
 declare HOST="https://api.cloudflare.com"
